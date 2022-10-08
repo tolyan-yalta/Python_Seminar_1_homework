25b437652396fe26326a1fa019a9b623ace5877e
anatoly <tolyan.yalta@yandex.ru>
2022-10-08 18:21:24 +0300

    File Task_1.py version 2.0

--- Task_1.py
+++ Task_1.py
@@ -1,13 +1,40 @@
 # Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
 # и проверяет, является ли этот день выходным.
 
-print('Введите цифру, обозначающую день недели:', end=' ')
-number = int(input())
+def is_int(value):  # Проверка введенного значения на целое число
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+def is_interval(value): # Проверка диапазона чисел
+    if is_int(value):
+        if 0 < int(value) < 8:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+number = input('Введите цифру, обозначающую день недели: ')
+
+while is_int(number) == False or is_interval(number) == False:  # Проверка условий для введенного значения
+    if is_int(number) == False:
+        print('Введенное значение не целое число')
+        number = input('Введите день недели: ')
+        continue
+    elif 0 < int(number) < 8:
+        break
+    else:
+        print('День недели может быть от 1 до 7')
+        number = input('Введите день недели: ')
+
+number = int(number)
 
 if 1 <= number <= 5:
     print('Нет, это не выходной')
-elif number == 6 or number == 7:
-    print('Да, это выходной')
 else:
-    print('Нет такого дня недели')
+    print('Да, это выходной')
+
 
